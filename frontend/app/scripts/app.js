f98d2f02db52b0813c1a1576bdcc65debf18d50d
wires <jelle@defekt.nl>
2014-09-13 17:19:02 +0200

    removed google shit

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -23,23 +23,11 @@ angular
 	.config(['$routeProvider',
 		function($routeProvider) {
 			$routeProvider
-				.when('/game', {
+				.when('/bar', {
 					templateUrl: 'partials/game.html',
-					controller: 'GameController'
-				})
-				.when('/log', {
-					templateUrl: 'partials/log.html',
-					controller: 'MyCtrl1'
-				})
-				.when('/workset', {
-					templateUrl: 'partials/workset.html',
-					controller: 'WorkSetController'
-				})
-				.when('/list', {
-					templateUrl: 'partials/list.html',
 					controller: 'ListController'
 				})
-				.otherwise({redirectTo: '/list'});
+				.otherwise({redirectTo: '/'});
 		}
 	]);
 })();

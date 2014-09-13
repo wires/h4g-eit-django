908c0d85d078629b56aeb5905b35b65bda7e409a
wires <jelle@defekt.nl>
2014-09-13 18:14:41 +0200

    scriptS

---
+++ frontend/app/scripts/fake.js
@@ -0,0 +1,10 @@
+
+var Issue = {
+	image_url: 'http://d.ibtimes.co.uk/en/full/1395234/ebola-liberia.jpg',
+	topic: 'label topic',
+	title: 'Ebola in Sierra Leone',
+	author: 'Topic author',
+	pitch: 'Issue description or pitch. Phasellus quis ligula ut enim pretium ornare nec nec est. Vestibulum eget ipsum fringilla, scelerisque velit vel, cursus eros. Curabitur ullamcorper interdum sapien quis pretium. In eget tristique massa. In lacinia risus sed ullamcorper bibendum. Sed consectetur nec sem at venenatis. Integer sed lacinia risus, eu malesuada lectus. In dignissim congue massa, eu eleifend dui tristique maximus. Sed vehicula ante ut mauris vehicula, non laoreet arcu dapibus.',
+	starred: true
+}
+

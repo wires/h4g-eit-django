e9fe9ea38bac8c2cd584e593140f760354ade353
jp.pequito <jp.pequito@cupenya.com>
2014-09-13 22:51:31 +0200

    list view

--- frontend/app/scripts/controllers.js
+++ frontend/app/scripts/controllers.js
@@ -6,6 +6,12 @@ angular.module('myApp.controllers', [])
 	.controller('ListController', ['$scope', '$http', '$location',
 		function($scope, $http, $location) {
 			$scope.foo = "hello";
+
+	      var msnry = new Masonry( "#container", {
+	        // options
+	        columnWidth: 140,
+	        itemSelector: '.item'
+	      });
 		}
 	])
 	.controller('IssueController', ['$scope', '$http', '$location',
